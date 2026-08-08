78d0d8f54c8aa952ba4db43605241fd69b75eae5
David <david_rpy85@hotmail.com>
2026-08-08 08:29:18 -0500

    quitando conexion con supa

--- config/configuracion.py
+++ config/configuracion.py
@@ -1,9 +1,9 @@
 SEMILLA = 27700
 MUESTRAS = 20000
 
-BACKEND_URL = "https://agroinvernaderobackend.onrender.com"
+# BACKEND_URL = "https://agroinvernaderobackend.onrender.com"
 
-ENDPOINT_SENSORES = "/api/sensores"
+# ENDPOINT_SENSORES = "/api/sensores"
 
 
 
