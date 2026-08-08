15d03fb2f945ddac0f2c13848e51a37754522e7a
David <david_rpy85@hotmail.com>
2026-08-07 20:37:34 -0500

    quitando conexion con supa

--- servicio/app.py
+++ servicio/app.py
@@ -1,92 +1,92 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel
+# from fastapi.middleware.cors import CORSMiddleware
 
-from autenticacionConexion.login import Login
-from main import ejecutar_modelo
-
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# JWT almacenado en memoria mientras la API esté ejecutándose
-jwt_actual = None
+# # from autenticacionConexion.login import Login
+# from main import ejecutar_modelo
 
 
-class LoginRequest(BaseModel):
-    email: str
-    password: str
+# app = FastAPI()
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:4200"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# # JWT almacenado en memoria mientras la API esté ejecutándose
+# jwt_actual = None
 
 
-@app.post("/login")
-def login(datos: LoginRequest):
-
-    global jwt_actual
-
-    try:
-
-        autenticacion = Login()
-
-        jwt_actual = autenticacion.iniciar_sesion(
-            datos.email,
-            datos.password
-        )
-
-        return {
-            "autenticado": True,
-            "mensaje": "Inicio de sesión exitoso"
-        }
-
-    except Exception as e:
-
-        jwt_actual = None
-
-        raise HTTPException(
-            status_code=401,
-            detail=str(e)
-        )
+# class LoginRequest(BaseModel):
+#     email: str
+#     password: str
 
 
-@app.post("/ejecutar")
-def ejecutar():
+# @app.post("/login")
+# def login(datos: LoginRequest):
 
-    global jwt_actual
+#     global jwt_actual
 
-    if jwt_actual is None:
+#     try:
 
-        raise HTTPException(
-            status_code=401,
-            detail="Debe iniciar sesión primero."
-        )
+#         autenticacion = Login()
 
-    try:
+#         jwt_actual = autenticacion.iniciar_sesion(
+#             datos.email,
+#             datos.password
+#         )
 
-        resultado = ejecutar_modelo(jwt_actual)
+#         return {
+#             "autenticado": True,
+#             "mensaje": "Inicio de sesión exitoso"
+#         }
 
-        return resultado
+#     except Exception as e:
 
-    except Exception as e:
+#         jwt_actual = None
 
-        raise HTTPException(
-            status_code=500,
-            detail=str(e)
-        )
+#         raise HTTPException(
+#             status_code=401,
+#             detail=str(e)
+#         )
 
 
-@app.post("/logout")
-def logout():
+# @app.post("/ejecutar")
+# def ejecutar():
 
-    global jwt_actual
+#     global jwt_actual
 
-    jwt_actual = None
+#     if jwt_actual is None:
 
-    return {
-        "autenticado": False,
-        "mensaje": "Sesión finalizada."
-    }
+#         raise HTTPException(
+#             status_code=401,
+#             detail="Debe iniciar sesión primero."
+#         )
+
+#     try:
+
+#         resultado = ejecutar_modelo(jwt_actual)
+
+#         return resultado
+
+#     except Exception as e:
+
+#         raise HTTPException(
+#             status_code=500,
+#             detail=str(e)
+#         )
+
+
+# @app.post("/logout")
+# def logout():
+
+#     global jwt_actual
+
+#     jwt_actual = None
+
+#     return {
+#         "autenticado": False,
+#         "mensaje": "Sesión finalizada."
+#     }
